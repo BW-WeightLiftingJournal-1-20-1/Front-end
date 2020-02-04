9f571a3cddff8f9219877913f30b61b9e3ed06fb
jkuenzinger <jkuenzinger228@gmail.com>
2020-02-04 14:09:00 -0800

    i got the inputs working and i got the validation working so you need 6 characters to get the password to not throw an error also got the uername ot return username required if nothing is typed in the field

--- weight-lifting/src/components/RegisterForm.js
+++ weight-lifting/src/components/RegisterForm.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
+import {withFormik, Form, Field, yupToFormErrors} from 'formik';
+import * as Yup from 'yup';
 
-const RegisterForm = () => {
-    return (
-        <>
-        </>
-      );
-    };
+const RegisterForm = ({values, errors, touched, status}) => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+}
     
-export default RegisterForm;
+return (
+  <div>
+    <div>
+       <h2>Sign Up</h2>
+          </div>
+      <Form onSubmit={handleSubmit}>
+          <Field
+            type="text"
+            name="username"
+            placeholder="username"
+          />
+            {touched.username && errors.username && (<p>{errors.username}</p>)}
+              <Field
+              type="password"
+              name="password"
+              placeholder="password"
+          />
+            {touched.password && errors.password && (<p>{errors.password}</p>) }
+    <div>
+            <button type="submit">Sign Up</button>
+  </div>
+        </Form>
+               </div>
+);
+};
+
+    const FormikRegisterForm = withFormik({
+      validationSchema: Yup.object().shape({
+        username: Yup.string().required('Username Required'),
+        password: Yup.string().min(6, 'Password needs to be at least 6 characters long').required('Password Required')
+      })
+    })(RegisterForm)
+  
+    
+export default FormikRegisterForm;
