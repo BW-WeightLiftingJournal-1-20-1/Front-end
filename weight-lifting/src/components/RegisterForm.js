445c5b0e89a00d095e4dea8ef64c5f0218c9e80a
jkuenzinger <jkuenzinger228@gmail.com>
2020-02-05 13:27:23 -0800

    did a little bit of styling trying to make the form work properly i am lost and don't know how to proceed

--- weight-lifting/src/components/RegisterForm.js
+++ weight-lifting/src/components/RegisterForm.js
@@ -7,19 +7,31 @@ const Signuptitle = styled.div`
 display:flex;
 flex-direction:column;
 align-items:center;
-margin-right:25%;
-margin-left:25%;
-width:50%;
+margin-right:15%;
+margin-left:15%;
+margin-top: 15%;
+width:70%;
+background-color: #07575B;
+padding-bottom:5%;
 `;
 
 const Submitbutton = styled.button`
 display:flex;
-width:30%;
-margin-left:35%;
-margin-right:35%;
+width:40%;
+margin-left:30%;
+margin-right:30%;
 margin-top:5%;
-
 `;
+
+const Title = styled.h2`
+color: #C4DFE6;
+`;
+
+const Headertitle = styled.h1`
+color: #66A5AD;
+`
+
+
 
 
 
@@ -30,25 +42,34 @@ const RegisterForm = ({values, errors, touched, status}) => {
   });
  const handleSubmit = event => {
     event.preventDefault();
+    
     }
 const handleChanges = event => {
     setUserState({...userState, [event.target.name]: event.target.value})
   
 }
 return (
+
+  
   <Signuptitle>
+      <div>
+<Headertitle>Weight Lifting Journal</Headertitle>
+    </div>
     <div>
-       <h2>Sign Up</h2>
+       <Title>Sign Up</Title>
           </div>
+         
       <Form onSubmit={handleSubmit}>
+      <div>
           <Field
             type="text"
             name="username"
             placeholder="username"
-             
             onChange={handleChanges}
            />
             {touched.username && errors.username && (<p>{errors.username}</p>)}
+            </div>
+              <div>
               <Field
               type="password"
               name="password"
@@ -57,6 +78,7 @@ return (
               onChange={handleChanges}
               />
             {touched.password && errors.password && (<p>{errors.password}</p>) }
+            </div>
       <div>
             <Submitbutton type="submit">Sign Up</Submitbutton>
       </div>
